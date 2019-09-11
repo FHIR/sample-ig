8d7f9967d8d97a551b1e9eda3567d1a6e2398b93
patrick-werner <pa.f.werner@gmail.com>
2019-09-11 16:30:14 -0400

    fixed IG update script

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "Downloading most recent publisher - it's ~100 MB, so this may take a bit"
-wget -P input-cache -N https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.publisher.jar
+wget https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.publisher.jar -O input-cache/org.hl7.fhir.publisher.jar
 echo "Done"
