debef4f74a17b25d3180db51c1a2edfd5327ee6e
patrick-werner <pa.f.werner@gmail.com>
2019-09-11 17:19:55 -0400

    test if wget is available

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+if ! type "wget" > /dev/null; then
+  echo "ERROR: Script needs wget to download latest IG Publisher. Please install wget."
+  exit 1
+fi
 echo "Downloading most recent publisher - it's ~100 MB, so this may take a bit"
 wget https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.publisher.jar -O input-cache/org.hl7.fhir.publisher.jar
 echo "Done"
