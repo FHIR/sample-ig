912b4a885a6bcd7d7723df24453bc3523831ee2d
patrick-werner <pa.f.werner@gmail.com>
2019-09-11 16:27:58 -0400

    corrected update script

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "Downloading most recent publisher - it's ~100 MB, so this may take a bit"
-wget -P -O input-cache https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.publisher.jar
+wget -P input-cache -N https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.publisher.jar
 echo "Done"
