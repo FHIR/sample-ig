91a388b08a7726383c354d9592f63c09d7a04048
patrick-werner <pa.f.werner@gmail.com>
2019-09-12 22:20:38 -0400

    switched the upgradescript wget -> curl

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-if ! type "wget" > /dev/null; then
-  echo "ERROR: Script needs wget to download latest IG Publisher. Please install wget."
+if ! type "curl" > /dev/null; then
+  echo "ERROR: Script needs curl to download latest IG Publisher. Please install curl."
   exit 1
 fi
 echo "Downloading most recent publisher - it's ~100 MB, so this may take a bit"
-wget https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.publisher.jar -O input-cache/org.hl7.fhir.publisher.jar
+curl https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.publisher.jar -o input-cache/org.hl7.fhir.publisher.jar --create-dirs
 echo "Done"
