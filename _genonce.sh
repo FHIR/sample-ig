abdedb571d41358b93c349cd8b8ace1610c317ad
Lloyd McKenzie <lmckenzie@gevityinc.com>
2019-12-19 09:36:13 -0700

    Added continuous scripts, incorporated Eric's ignore stuff

--- _genonce.sh
+++ _genonce.sh
@@ -17,13 +17,13 @@ echo "$txoption"
 
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	JAVA -jar $publisher -ig ig.ini $txoption
+	JAVA -jar $publisher -ig ig.ini $txoption $*
 
 else
 	publisher=../$publisher_jar
 	if test -f "$publisher"; then
-		JAVA -jar $publisher -ig ig.ini $txoption
+		JAVA -jar $publisher -ig ig.ini $txoption $*
 	else
-		echo IG Publisher NOT FOUND in input-cache or parent folder... aborting
+		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 	fi
 fi
